940615255fbcd0497a7c3cb8e7da22405107528d
Triet0211 <triet02112001@gmail.com>
2022-09-22 13:37:32 +0700

    delete redundant file

--- dist/common/utils/SerializerUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toJSONAnInstance = void 0;
-function toJSONAnInstance(instance) {
-    return JSON.parse(JSON.stringify(instance));
-}
-exports.toJSONAnInstance = toJSONAnInstance;
-//# sourceMappingURL=SerializerUtils.js.map
